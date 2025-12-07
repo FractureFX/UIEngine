3fd7cd047cb64b3053e60f9cda805fed5b6674a2
FractureFX <leg0rmail@gmail.com>
2025-12-07 15:01:33 -0500

    Pre-release commit #4

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/PauseLayer.hpp>
+#include <Geode/ui/Notification.hpp>
 
 using namespace geode::prelude;
 
@@ -23,6 +25,29 @@ class $modify(MyMenuLayer, MenuLayer) {
 	}
 
 	void onButtonClick(CCObject* sender) {
-		FLAlertLayer::create("The Engine is WIP", "Still coding this lol", "OK")->show();
+		auto notif = Notification::create("Entered the UI Engine!", NotificationIcon::Success, float(3.0f));
+		notif->show();
+	}
+};
+
+class $modify(MyPauseLayer, PauseLayer) {
+	void customSetup() {
+		PauseLayer::customSetup();
+		auto menu = this->getChildByID("right-button-menu");
+		if (!menu) return;
+		auto spr = CCSprite::createWithSpriteFrameName("GJ_editBtn_001.png");
+		spr->setScale(0.65f);
+		auto psbtn = CCMenuItemSpriteExtra::create(
+			spr, 
+			this,
+			menu_selector(MyPauseLayer::onButtonClick)
+		);
+		menu->addChild(psbtn);
+		menu->updateLayout();
+	}
+
+	void onButtonClick(CCObject* sender) {
+		auto notif = Notification::create("Entered the UI Engine!", NotificationIcon::Success, float(3.0f));
+		notif->show();
 	}
 };
